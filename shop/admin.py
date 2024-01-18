e3a74d316368532f05db72eac240723affc21071
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-01-19 00:46:53 +0300

    created models for organization

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from shop.models import Contact, Product
-
+from shop.models import Contact, Product, Organization
 
 admin.site.register(Contact)
 admin.site.register(Product)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+	list_display = [f.name for f in Organization._meta.fields]
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from user.models import NULLABLE
@@ -20,7 +21,7 @@ class Contact(models.Model):
 		verbose_name = "Контакт"
 		verbose_name_plural = "Контакты"
 		db_table = 'contacts'
-		unique_together = (('email', 'country', 'city', 'name'),)
+		unique_together = (('email', 'country', 'city', 'street', 'house_number'),)
 
 
 class Product(models.Model):
@@ -32,10 +33,60 @@ class Product(models.Model):
 	release_date = models.DateField(max_length=50, verbose_name='Дата выхода на рынок', **NULLABLE)
 
 	def __str__(self):
-		return f"{self.name} {self.model}"
+		return self.name
 
 	class Meta:
 		verbose_name = "Продукт"
 		verbose_name_plural = "Продукты"
 		db_table = 'products'
 		unique_together = (('name', 'model'),)
+
+
+class OrganizationChoices(models.TextChoices):
+
+	FACTORY = 'F', 'Завод'
+	RETAIL = 'R', 'Розничная сеть'
+	IE = 'IE', 'Индивидуальный предприниматель'
+
+
+class Organization(models.Model):
+	organization_type = models.CharField(
+		max_length=2, choices=OrganizationChoices.choices, verbose_name='Тип организации'
+	)
+	hierarchy_level = models.PositiveSmallIntegerField(verbose_name='Уровень в иерархии', **NULLABLE)
+	name = models.CharField(unique=True, max_length=50, verbose_name='Название организации')
+	contacts = models.ForeignKey(
+		Contact, on_delete=models.CASCADE, related_name='contacts', verbose_name='Контакты'
+	)
+	supplier = models.ForeignKey(
+		'self', on_delete=models.CASCADE, related_name='suppliers', verbose_name='Поставщик', **NULLABLE
+	)
+	product = models.ManyToManyField(
+		Product, related_name='products', verbose_name=''
+	)
+	debt = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name='Долг перед поставщиком')
+	created_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+	class Meta:
+		verbose_name = "Организация"
+		verbose_name_plural = "Организации"
+		db_table = 'organizations'
+
+	def __str__(self):
+		return self.name
+
+	def clean(self):
+		if self.organization_type == OrganizationChoices.FACTORY and self.supplier:
+			raise ValidationError("Factory doesn't have suppliers")
+
+		if not self.supplier and self.debt > 0:
+			raise ValidationError("Factory can't have a dept")
+
+	def save(self, *args, **kwargs):
+		if self.organization_type == OrganizationChoices.FACTORY:
+			self.hierarchy_level = 0
+		elif self.supplier and self.supplier.hierarchy_level == 0:
+			self.hierarchy_level = 1
+		else:
+			self.hierarchy_level = 2
+		super().save(*args, **kwargs)
